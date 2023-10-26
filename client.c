0e92dd47a1e2b88a16f3bed029a799df394377e7
ThalisonCosta <thalisoncosta123@gmail.com>
2023-10-25 22:27:42 -0400

    connect to localhost

--- client.c
+++ client.c
@@ -6,18 +6,15 @@
 #include <sys/socket.h>
 
 int main(){
-  int sock_fd;
-  struct sockaddr_in server;
-  char *message, server_reply[2048];
-
-  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (sock_fd == -1) {
     puts("failed creat socket\n");
     exit(1);
   }
 
-  server.sin_addr.s_addr = inet_addr("172.217.166.46");
-  server.sin_port = htons(80);
+  struct sockaddr_in server;
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_port = htons(4444);
   server.sin_family = AF_INET;
 
   if (connect(sock_fd, (struct sockaddr *)&server, sizeof(server)) <0 ) {
@@ -26,7 +23,7 @@ int main(){
   }
   puts("connected");
 
-  message = "GET / HTTP/1.1\r\n\r\n";
+  char *message = "GET / HTTP/1.1\r\n\r\n";
 
   if (send(sock_fd, message, strlen(message), 0) < 0) {
     puts("failed send message\n");
@@ -34,6 +31,7 @@ int main(){
   }
   puts("data sent\n");
 
+  char server_reply[2048];
   if (recv(sock_fd, server_reply, 2048, 0) < 0) {
     puts("failed to receive message\n");
     exit(1);
